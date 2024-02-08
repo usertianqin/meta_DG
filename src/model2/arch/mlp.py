bb3755f6d0485c1454f4d61a2b88f95c1031f7f2
usertianqin <tqintju@163.com>
2024-02-08 13:02:02 +0800

    Add files via upload

--- src/model2/arch/mlp.py
+++ src/model2/arch/mlp.py
@@ -33,10 +33,7 @@ class Linear(nn.Module):
         self.reset_parameters()
 
     def reset_parameters(self) -> None:
-        #nn.init.kaiming_uniform_(self.weight, a=math.sqrt(5))
-        #nn.init.normal_(self.weight, 0., 1e-2)
         nn.init.xavier_uniform_(self.weight, gain=1.0)
-        #nn.init.trunc_normal_(self.weight, mean=0.0, std=1.0, a=-2.0, b=2.0)
         if self.bias is not None:
             fan_in, _ = nn.init._calculate_fan_in_and_fan_out(self.weight)
             bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
@@ -91,7 +88,6 @@ class MLP(MLPBase):
     def forward(self, x): return self.f_x2y(x).squeeze(-1)
 
 class MLPsvy1x(MLPBase):
-    #MLPsvy1x(dim_x=dim_x, dim_y=dim_y, std_v1x_val=std_v1x_val, std_s1vx_val=std_s1vx_val,after_actv=after_actv, **stru)
     def __init__(self, dim_x, dims_postx2prev, dim_v, dim_parav, dims_postv2s, dims_posts2prey, dim_y, actv = "Sigmoid",
             std_v1x_val: float=1., std_s1vx_val: float=-1., after_actv: bool=True): # if <= 0, then learn the std.
         """
@@ -103,7 +99,6 @@ class MLPsvy1x(MLPBase):
         super(MLPsvy1x, self).__init__()
         self.vars = nn.ParameterList()
         self.graph_conv = []
-        #self.new_vars = nn.ParameterList()
         
         if type(actv) is str: actv = getattr(nn, actv)
         self.dim_x, self.dim_v, self.dim_y = dim_x, dim_v, dim_y
@@ -139,12 +134,11 @@ class MLPsvy1x(MLPBase):
         self.graph_conv.append(f_s2y)
         
         
-        #list = [f_prev2v[0].weight.data, f_prev2v[0].bias.data, f_prev2parav[0].weight.data, f_prev2parav[0].bias.data, f_vparav2s[0].weight.data, f_vparav2s[0].bias.data, f_s2y[0].weight.data, f_s2y[0].bias.data]
+        
         list = [f_prev2v[0].weight.data, f_prev2v[0].bias.data, f_prev2parav[0].weight.data, f_prev2parav[0].bias.data, f_vparav2s[0].weight.data, f_vparav2s[0].bias.data]
         for item in list:
             self.vars.append(item)
-        # self.vars.append([self.f_prev2v[0].weight.data, self.f_prev2v[0].bias.data, self.f_vparav2s[0].weight.data, self.f_vparav2s[0].bias.data, self.f_s2y[0].weight.data, self.f_s2y[0].bias.data, self.f_prev2parav[0].weight.data, self.f_prev2parav[0].bias.data, self.f_vparav2s[0].weight.data, self.f_vparav2s[0].bias.data, self.f_s2y[0].weight.data, self.f_s2y[0].bias.data])
-        
+       
         
         self.std_v1x_val = std_v1x_val; self.std_s1vx_val = std_s1vx_val
         self.learn_std_v1x = std_v1x_val <= 0 if type(std_v1x_val) is float else (std_v1x_val <= 0).any()
@@ -154,23 +148,6 @@ class MLPsvy1x(MLPBase):
         self._v_cache = self._x_cache_v = None
         self._parav_cache = self._x_cache_parav = None
 
-        ## std models
-        #print("self.learn_std_v1x", self.learn_std_v1x)
-        # if self.learn_std_v1x:
-        #     nn_std_v = nn.Sequential(
-        #             mlp_constructor(
-        #                 [dim_prev, dim_v], #dim_prev 是个list包含输入维度和各层隐藏维度， div-v是输出维度
-        #                 nn.ReLU, lastactv = False),
-        #             nn.Softplus() #激活函数
-        #         )
-        #     init_linear(nn_std_v, 0., 1e-2, 0.)
-        #     f_std_v = nn_std_v
-            
-        # self.graph_conv.append(nn_std_v)
-        #self.vars.append(nn_std_v[0][0].weight.data)
-        #self.vars.append(nn_std_v[0][0].bias.data)
-        
-        #self.graph_conv.append(f_std_v)
         
         if self.learn_std_s1vx:
             self.nn_std_s = nn.Sequential(
@@ -228,14 +205,7 @@ class MLPsvy1x(MLPBase):
             return v
         else:
             return tensorify(x.device, self.std_v1x_val)[0].expand(x.shape[:-1]+(self.dim_v,))
-            #f_std_v = self.graph_conv[6][0][0]
-            #v = f_std_v(prev, vars[10], vars[11]) #f_std_v 400*100
-            #v = self.graph_conv[6][1](v)
-            #v =self.f_std_v(prev)
-           # print("v in std_v1x:", v)
-            #mod.weight.data.normal_(wmean, wstd)
-            #mod.bias.data.fill_(bval)
-            #init_linear(f_prev2v, 0., 1e-2, 0.)   
+
        
 
     def _get_parav(self, x, vars=None):
@@ -256,14 +226,13 @@ class MLPsvy1x(MLPBase):
         f_vparav2s = self.graph_conv[3][0]
         s = f_vparav2s(tc.cat([v, parav], dim=-1), vars[6], vars[7]) 
         s = self.graph_conv[3][1](s) 
-        #print("s:", s)
-        #print("v")
+
         return s
     
     def std_s1vx(self, v, x, vars=None):
         if vars is None:
             vars = self.vars
-        #print("self.learn_std_s1vx:",self.learn_std_s1vx)
+      
         if self.learn_std_s1vx:
             
             parav = self._get_parav(x, vars)
@@ -285,10 +254,9 @@ class MLPsvy1x(MLPBase):
     def y1s(self, s):
         
         f_s2y = self.graph_conv[4][0].to(s.device)
-        #print(f_s2y.parameters().device)
-        #print(s.device)
+   
         y = f_s2y(s).squeeze(-1)
-        return y # squeeze for binary y
+        return y 
 
     def ys1x(self, x, vars=None):
         if vars is None:
@@ -300,19 +268,17 @@ class MLPsvy1x(MLPBase):
         if vars is None:
             vars = self.vars
     
-        #return self.y1s(self.s1x(x))
  
         x2prev = self.graph_conv[0][0]
-        #x2prev[0].weight.data = vars[0]
-        #x2prev[0].bias.data = vars[1]
+  
         if not is_same_tensor(x, self._x_cache_prev):
             self._x_cache_prev = x
             self._prev_cache = x2prev(x, vars[0], vars[1])
-        prev = self.graph_conv[0][1](self._prev_cache) #激活函数
+        prev = self.graph_conv[0][1](self._prev_cache) 
         
         
         f_prev2v = self.graph_conv[1][0]
-        if not is_same_tensor(x, self._x_cache_v):#执行
+        if not is_same_tensor(x, self._x_cache_v):
             self._x_cache_v = x
             self._v_cache = f_prev2v(prev, vars[2], vars[3])
         v = self.graph_conv[1][1](self._v_cache)
@@ -326,13 +292,6 @@ class MLPsvy1x(MLPBase):
         f_vparav2s = self.graph_conv[3][0]
         s = f_vparav2s(tc.cat([v, parav], dim=-1), vars[6], vars[7]) 
         s =    self.graph_conv[3][1](s)
-        #print("s:", s)
-        #print("v:", parav)
-        #f_s2y = self.graph_conv[4][0]
-        #y = f_s2y(s, vars[8], vars[9]).squeeze(-1)
-        
-
-       # return y, s, v
         return s, v
     
     def parameters(self):
@@ -357,8 +316,7 @@ class MLPx1sv(MLPBase):
         super(MLPx1sv, self).__init__()
         self.para = nn.ParameterList()
         self.graph_conv = []
-        #self.new_vars = None
-        
+     
         self.dim_s, self.dim_v, self.dim_x = dim_s, dim_v, dim_x
         self.dims_pres2parav, self.dims_prev2postx, self.actv = dims_pres2parav, dims_prev2postx, actv
         f_s2parav = mlp_constructor([dim_s] + dims_pres2parav, actv)
@@ -369,7 +327,7 @@ class MLPx1sv(MLPBase):
         f_vparav2x = mlp_constructor([dim_v + dims_pres2parav[-1]] + dims_prev2postx + [dim_x], actv)
 
         self.graph_conv.append(f_vparav2x)
-        #self.graph_conv.append(self.f_vparav2x[2])
+       
         
         self.para.append(f_vparav2x[0].weight.data)
         self.para.append(f_vparav2x[0].bias.data)
@@ -378,18 +336,16 @@ class MLPx1sv(MLPBase):
         
 
     def x1sv(self, s, v, vars=None): 
-        #print("x1sv in gen:")
+   
         if vars is None:
             vars = self.para
             idx = 0
         else:
             idx = 10
             
-        #print("vars in gen:", vars)
-        
         f_s2parav = self.graph_conv[0][0]
         s2parav = f_s2parav(s, vars[idx], vars[idx+1])
-        s2parav = self.graph_conv[0][1](s2parav) #激活函数
+        s2parav = self.graph_conv[0][1](s2parav) 
         
         f_vparav2x_1 = self.graph_conv[1][0]
         vparav2x_1 = f_vparav2x_1(tc.cat([v, s2parav], dim=-1), vars[idx+2], vars[idx+3])
@@ -398,7 +354,7 @@ class MLPx1sv(MLPBase):
         f_vparav2x_2 = self.graph_conv[1][2]
         x = f_vparav2x_2(vparav2x_1, vars[idx+4], vars[idx+5])
         x = self.graph_conv[1][3](x)
-        #print("x in gen", x)   
+ 
         
           
         return x
@@ -410,12 +366,10 @@ class MLPx1sv(MLPBase):
             idx=0
         else:
             idx = 8
-        #print("vars in gen forward:", vars)
-        
-        #self.new_vars = vars
+
         f_s2parav = self.graph_conv[0][0]
         s2parav = f_s2parav(s, vars[idx], vars[idx+1])
-        s2parav = self.graph_conv[0][1](s2parav) #激活函数
+        s2parav = self.graph_conv[0][1](s2parav) 
         
         f_vparav2x_1 = self.graph_conv[1][0]
         vparav2x_1 = f_vparav2x_1(tc.cat([v, s2parav], dim=-1), vars[idx+2], vars[idx+3])
@@ -424,7 +378,7 @@ class MLPx1sv(MLPBase):
         f_vparav2x_2 = self.graph_conv[1][2]
         x = f_vparav2x_2(vparav2x_1, vars[idx+4], vars[idx+5])
         x = self.graph_conv[1][3](x)
-        #print("x in gen forward:", x)
+   
         return x
     
     def parameters(self):
